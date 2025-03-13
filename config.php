6f5202261c99f81823bfe550bf020c5aac9aee67
reso <resat.bulut@ozu.edu.tr>
2025-03-13 13:58:27 +0000

    changed config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
+// Database configuration
+$db_host = '34.134.226.54';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = '123';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
